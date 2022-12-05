ebc795f26abd68735f276d440d766e8858a0e523
yh2n <yohann.potico@gmail.com>
2022-12-05 12:32:21 -0500

    Implement filters and chart zoom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,11 @@
+import { useState } from "react"
+import moment from "moment"
+import Highcharts from "highcharts/highstock"
+import HighchartsReact from "highcharts-react-official"
+import { Select } from "@chakra-ui/react"
+
+import "./App.css"
+
 function App() {
   const [state, setState] = useState(() => {
     fetch("http://localhost:8000/api", {
@@ -28,21 +36,37 @@ function App() {
           }
         }
         setState(trades)
-        console.table(trades[trades.length - 1])
-        console.log(
-          format(
-            fromUnixTime(1666019586954552 / 1000000),
-            "MM/dd/yyyy, HH:mm:ss"
-          )
-        )
       })
   })
+
+  const [filterOption, setFilterOption] = useState(["exchange", "OKEX"])
+
+  const handleFilterSelection = (e) => {
+    console.log(e.target.value)
+    let option = e.target.value.split(",")
+    console.log(option)
+    setFilterOption(option)
+    console.log(filterOption[0])
+  }
+  let selectOptions
+  if (state) {
+    let result = {}
+    for (let i = 0; i < state.length; ++i) {
+      result[state[i].exchange] = "exchange"
+      result[state[i].symbol] = "symbol"
+    }
+    selectOptions = Array.from(Object.entries(result))
+    // console.log(result)
+    // console.log(selectOptions)
+  }
 
   let options
   if (state) {
     options = {
       title: {
-        text: `${moment(1666019586954552 / 1000).format("MM/DD/YYYY")} Trades`,
+        // text: `${moment(1666019586954552 / 1000).format("MM/DD/YYYY")} Trades`,
+        text: `${filterOption[1]}: ${filterOption[0]}`,
+        style: { color: "white" },
       },
       series: [
         {
@@ -50,18 +74,25 @@ function App() {
           name: "BOUGHT",
           showInLegend: true,
           data: state
-            .filter((fill) => fill.side === "BUY" && fill.exchange === "OKEX")
+            .filter(
+              (fill) =>
+                fill.side === "BUY" && fill[filterOption[1]] === filterOption[0]
+            )
             .map((fill) => {
               return { x: fill.timestamp, y: fill.total }
             }),
-          color: "orange",
+          color: "purple",
         },
         {
           name: "SOLD",
           showInLegend: true,
           clip: false,
           data: state
-            .filter((fill) => fill.side === "SELL" && fill.exchange === "OKEX")
+            .filter(
+              (fill) =>
+                fill.side === "SELL" &&
+                fill[filterOption[1]] === filterOption[0]
+            )
             .map((fill) => {
               return { x: fill.timestamp, y: fill.total }
             }),
@@ -69,12 +100,10 @@ function App() {
         },
       ],
       xAxis: {
-        categories: state.map((fill) => fill.timestamp),
-        type: "datetime",
-        // visible: false,
         time: {
           timezone: "America/New_York",
         },
+        style: { color: "white" },
       },
       yAxis: {
         title: {
@@ -85,25 +114,34 @@ function App() {
       rangeSelector: {
         // enabled: false,
         verticalAlign: "top",
-        buttons: [{ type: "day", count: 1, text: "1 d" }],
+        buttons: [
+          { type: "minute", count: 5, text: "5m" },
+          { type: "minute", count: 10, text: "10m" },
+          { type: "minute", count: 15, text: "15m" },
+          { type: "minute", count: 30, text: "30m" },
+          { type: "hour", count: 1, text: "1h" },
+          { type: "all", text: "All" },
+        ],
       },
       chart: {
-        // type: "spline",
         height: "800px",
         backgroundColor: "black",
       },
       plotOptions: {
         series: {
-          cropThreshold: 800,
+          cropThreshold: 1500,
+          states: {
+            inactive: {
+              opacity: 0.8,
+            },
+          },
           tooltip: {
-            followPointer: true,
-            shared: true,
+            // followPointer: true,
+            // shared: true,
           },
         },
       },
       tooltip: {
-        split: true,
-        shared: true,
         borderColor: "silver",
         backgroundColor: "black",
         style: { color: "white" },
@@ -138,6 +176,24 @@ function App() {
           ) : null}
         </div>
       </section>
+      <div className="filter">
+        {state ? (
+          <Select
+            onChange={(e) => handleFilterSelection(e)}
+            bg="tomato"
+            borderColor="tomato"
+            color="white"
+          >
+            {selectOptions.map((option) => {
+              return (
+                <option value={[option]} key={option[0]}>
+                  {option[0]}
+                </option>
+              )
+            })}
+          </Select>
+        ) : null}
+      </div>
       <section className="chart">
         {state ? (
           <HighchartsReact
@@ -151,3 +207,5 @@ function App() {
     </div>
   )
 }
+
+export default App
